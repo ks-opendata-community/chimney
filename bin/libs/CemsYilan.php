6cd6fc5ff9c06041a54e4e0225faf553273d4903
kiang <kiange@gmail.com>
2016-01-29 02:00:17 +0800

    fix for missing daily data

--- bin/libs/CemsYilan.php
+++ bin/libs/CemsYilan.php
@@ -25,56 +25,56 @@ class CemsYilan {
         $dayUrl = date('Ymd', $currentTime);
         $timeIndexed = array();
 
-        if (date('Ymd') === date('Ymd', $currentTime)) {
-            $baseUrl = $this->baseUrl . 'Realtime/';
-        } else {
-            $baseUrl = $this->baseUrl . 'Daily/';
-        }
+        $dataTypes = array('Realtime', 'Daily');
         foreach ($this->factories AS $factory) {
-            $data = array();
-            $tmpFile = $dayPath . '/' . $factory;
-            if (!file_exists($tmpFile)) {
-                $url = $baseUrl . $factory . '/ALL/' . $dayUrl . '/Csv';
-                error_log("getting {$url}");
-                $curl = curl_init($url);
-                curl_setopt($curl, CURLOPT_REFERER, 'http://cems.ilepb.gov.tw/OpenData/');
-                curl_setopt($curl, CURLOPT_FORBID_REUSE, true);
-                curl_setopt($curl, CURLOPT_COOKIESESSION, true);
-                curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36');
-                curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($curl, CURLOPT_HEADER, 1);
-                $response = curl_exec($curl);
-                $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
-                $header = substr($response, 0, $header_size);
-                $content = substr($response, $header_size);
-                error_log("getting {$header}");
-                file_put_contents($tmpFile, $content);
-            }
-            $tmpFh = fopen($tmpFile, 'r');
-            fgetcsv($tmpFh, 2048);
-            /*
-             * Array
-              (
-              [0] => ﻿CNO
-              [1] => POLNO
-              [2] => TIME
-              [3] => ITEM
-              [4] => CODE2
-              [5] => VAL
-              )
-             */
-            while ($line = fgetcsv($tmpFh, 2048)) {
-                $timeKey = substr($line[2], 0, 2) . substr($line[2], -2);
-                if (!isset($timeIndexed[$timeKey])) {
-                    $timeIndexed[$timeKey] = array();
+            foreach ($dataTypes AS $dataType) {
+                $baseUrl = $this->baseUrl . $dataType . '/';
+                $data = array();
+                $tmpFile = $dayPath . '/' . $dataType . '_' . $factory;
+                if (!file_exists($tmpFile)) {
+                    sleep(1);
+                    $url = $baseUrl . $factory . '/ALL/' . $dayUrl . '/Csv';
+                    error_log("getting {$url}");
+                    $curl = curl_init($url);
+                    curl_setopt($curl, CURLOPT_REFERER, 'http://cems.ilepb.gov.tw/OpenData/');
+                    curl_setopt($curl, CURLOPT_FORBID_REUSE, true);
+                    curl_setopt($curl, CURLOPT_COOKIESESSION, true);
+                    curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36/' . uniqid());
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($curl, CURLOPT_HEADER, 1);
+                    $response = curl_exec($curl);
+                    $header_size = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+                    $header = substr($response, 0, $header_size);
+                    $content = substr($response, $header_size);
+                    error_log("getting {$header}");
+                    file_put_contents($tmpFile, $content);
                 }
-                $timeIndexed[$timeKey][] = array(
-                    $line[0],
-                    $line[1],
-                    $line[3],
-                    $timeKey,
-                    $line[5],
-                );
+                $tmpFh = fopen($tmpFile, 'r');
+                fgetcsv($tmpFh, 2048);
+                /*
+                 * Array
+                  (
+                  [0] => ﻿CNO
+                  [1] => POLNO
+                  [2] => TIME
+                  [3] => ITEM
+                  [4] => CODE2
+                  [5] => VAL
+                  )
+                 */
+                while ($line = fgetcsv($tmpFh, 2048)) {
+                    $timeKey = substr($line[2], 0, 2) . substr($line[2], -2);
+                    if (!isset($timeIndexed[$timeKey])) {
+                        $timeIndexed[$timeKey] = array();
+                    }
+                    $timeIndexed[$timeKey][] = array(
+                        $line[0],
+                        $line[1],
+                        $line[3],
+                        $timeKey,
+                        $line[5],
+                    );
+                }
             }
         }
         ksort($timeIndexed);
